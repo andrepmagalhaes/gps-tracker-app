419961c040a9ce8ab751c5e36984c3db7171bfbd
andrepmagalhaes <andrepimentelm@gmail.com>
2021-05-09 08:51:25 -0300

    deploy test3

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var App_1 = __importDefault(require("./App"));
 var AppRouter_1 = __importDefault(require("./AppRouter"));
 var arduinoRoutes_1 = __importDefault(require("./controllers/arduinoRoutes"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 var app = new App_1.default(new AppRouter_1.default([
     arduinoRoutes_1.default
-]), 5000);
+]), (Number(process.env.PORT) || 8000));
 app.test();
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import App from './App';
 import AppRouter from './AppRouter';
 import arduinoRoutes from './controllers/arduinoRoutes';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = new App(
     new AppRouter([
         arduinoRoutes
     ]),
-    5000
+    (Number(process.env.PORT) || 8000)
 );
 
 app.test();
